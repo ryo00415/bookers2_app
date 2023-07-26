9f0cc10df4d1940eea77dcb21d0b2cd78c932867
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-07-26 10:58:18 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get "/home/about" => "homes#about", as: "about"
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :index, :show, :edit, :update, :create, :destroy]
+end
